ff3955e9134743c0c3aef66463a9cb40844259f0
Dkell88 <DarrenKelly1188@gmail.com>
2022-04-11 19:26:38 -0700

    Hoisting is a thing

--- spinner2.js
+++ spinner2.js
@@ -3,12 +3,13 @@ if (process.argv[2]) {
   numOfRotations = process.argv[2];
 }
 let spinnerChars = ['\r|   ','\r/   ','\r-   ', '\r\\   ', '\r|   ', '\r/   ','\r-   ','\r\\  ' ];
-for (let i = 0; i < 8 * numOfRotations; i ++) {
-  spinner(i);
-}
-const spinner = function(i, time) {
+
+const spinner = function(i) {
   setTimeout(() => {
     process.stdout.write(spinnerChars[i % 7]);
   }, (i * 200 + 100));
 };
 
+for (let i = 0; i < 8 * numOfRotations; i ++) {
+  spinner(i);
+}
